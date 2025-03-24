da6da7a682e7b830093cc3784ff6bfd6283045ca
vrushalip049 <45143287+vrushalip049@users.noreply.github.com>
2025-03-24 16:14:36 +0530

    2nd commit

--- src/test/java/qa/test/PracticeFormTest.java
+++ src/test/java/qa/test/PracticeFormTest.java
@@ -38,14 +38,43 @@ public class PracticeFormTest {
 
 		Pattern firstNameTextBox = new Pattern(filepath + "FirstName.png");
 		Pattern lastNameTextBox = new Pattern(filepath + "LastName.png");
-		
+		Pattern ChooseFileButton = new Pattern(filepath + "ChooseFileButton.png");
+		Pattern CloseButton = new Pattern(filepath + "CloseButton.png");
+		Pattern CurrentAddress = new Pattern(filepath + "CurrentAddress.png");
+		Pattern DateOfBirth = new Pattern(filepath + "DateOfBirth.png");
+		Pattern Email = new Pattern(filepath + "Email.png");
+		Pattern FemaleRadioButton = new Pattern(filepath + "FemaleRadioButton.png");
+		Pattern MaleRadioButton = new Pattern(filepath + "MaleRadioButton.png");
+		Pattern MusicCheckbox = new Pattern(filepath + "MusicCheckbox.png");
+		Pattern OtherRadioButton = new Pattern(filepath + "OtherRadioButton.png");
+		Pattern ReadingCheckbox = new Pattern(filepath + "ReadingCheckbox.png");
+		Pattern SelectState = new Pattern(filepath + "SelectState.png");
+		Pattern SportsCheckBox = new Pattern(filepath + "SportsCheckBox.png");
+		Pattern Subject = new Pattern(filepath + "Subject.png");
+		Pattern SubmitButton = new Pattern(filepath + "SubmitButton.png");
+		Pattern ThanksForSubmitMessage = new Pattern(filepath + "ThanksForSubmitMessage.png");
+		Pattern MobileNumber = new Pattern(filepath + "MobileNumber.png");
 		Screen s = new Screen();
-	
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	
 		try {
 			s.type(firstNameTextBox,"Vrushali");
 			
 			s.type(lastNameTextBox,"Patil");
+			
+			s.type(Email,"vp@gmail.com");
+			
+			s.click(FemaleRadioButton);
+			
+			
+			s.type(MobileNumber,"1234567890");
+			
+			
 		} catch (FindFailed e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
